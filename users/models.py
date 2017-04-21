b42d321bc04b6dc549f137b2117a0bc14c8de308
WillGiab <2410250284@qq.com>
2017-04-22 00:31:11 +0800

    users app和models的设计
    一个UserProfile类和一个EmailverifyRecord类。
    还踩了一个mysql编码问题的坑

---
+++ users/models.py
@@ -0,0 +1,37 @@
+#_*_ encoding:utf-8 _*_
+from __future__ import unicode_literals
+from datetime import datetime
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+
+
+class UserProfile(AbstractUser):
+    nick_name = models.CharField(max_length=50,verbose_name=u"昵称",default="")
+    birday = models.DateField(verbose_name=u"生日",null=True,blank=True)
+    gender = models.CharField(max_length=6,choices=(("mule",u"男"),("femule",u"女"),("unknow",u"保密")),default="unknow")
+    address = models.CharField(max_length=100,default=u"")
+    mobile = models.CharField(max_length=11,null=True,blank=True)
+    image = models.ImageField(upload_to="image/%Y/%m",default=u"image/default.png",max_length=100)
+
+    class Meta:
+        verbose_name = u"用户信息"
+        verbose_name_plural = verbose_name
+
+    def __unicode__(self):
+        return self.username
+
+
+#邮箱验证码
+class EmailVerifyRecord(models.Model):
+    code = models.CharField(max_length=8,verbose_name=u"验证码")
+    email = models.EmailField(max_length=50,verbose_name=u"邮箱")
+    send_type = models.CharField(choices=(("register",u"注册"),("forget",u"找回密码")),max_length=10,default="register")
+    send_time = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        verbose_name= u"邮件验证码"
+        verbose_name_plural = verbose_name
