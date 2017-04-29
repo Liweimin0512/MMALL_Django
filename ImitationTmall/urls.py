944b72f59ca615c470980bf82b177acdf27fb8c7
GamemakerGiab <2410250284@qq.com>
2017-04-29 10:48:48 +0800

    创建项目
    昨天的不算，重新创建了项目

--- ImitationTmall/urls.py
+++ ImitationTmall/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'tmall.views.home', name='home'),
+    # url(r'^$', 'ImitationTmall.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
