9d3b4021ef7a6ffe404df83ff28e5239aad6d009
WillGiab <2410250284@qq.com>
2017-05-24 23:44:56 +0800

    xadmin加强

--- apps/operation/adminx.py
+++ apps/operation/adminx.py
@@ -5,11 +5,18 @@ __date__ = '2017/4/22 17:52'
 import xadmin
 from .models import Order,OrderItem
 
+
 class OrderAdmin(object):
-    pass
+    readonly_fields = ["orderCode", "address", "post",
+                       "receiver", "mobile", "userMessage",
+                       "createDate", "payDate", "confirmDate",
+                       "user"]
+
 
 class OrderItemAdmin(object):
     pass
 
-xadmin.site.register(Order,OrderAdmin)
-xadmin.site.register(OrderItem,OrderItemAdmin)
+xadmin.site.register(Order, OrderAdmin)
+xadmin.site.register(OrderItem, OrderItemAdmin)
+
+
